a16b08be5dbb5a9efeb531645bb26efbef1fcaa5
schwallsunk <124298837+schwallsunk@users.noreply.github.com>
2026-09-18 22:04:31 +0200

    Refactor signal discriminator module with comments
    Updated comments for clarity and added unused input handling.

--- src/project.v
+++ src/project.v
@@ -1,12 +1,12 @@
 /*
-* Copyright (c) 2024 Schwallsunk
+ * Copyright (c) 2024 Schwallsunk
  * SPDX-License-Identifier: Apache-2.0
  */
 
 `default_nettype none
 
 module tt_um_schwallsunk_signal_discriminator (
-    input  wire [7:0] ui_in,    // Dedicated inputs
+    input  wire [7:0] ui_in,    // Dedicated inputs: 0 = low thresh, 1 = high thresh, 2 = MUX IO DLY 0, 3 = MUX IO DLY 1, 4 = shift to buffer
     output wire [7:0] uo_out,   // Dedicated outputs
     input  wire [7:0] uio_in,   // IOs: Input path
     output wire [7:0] uio_out,  // IOs: Output path
@@ -15,13 +15,107 @@ module tt_um_schwallsunk_signal_discriminator (
     input  wire       clk,      // clock
     input  wire       rst_n     // reset_n - low to reset
 );
+    assign uio_oe  = 8'b11111111; // port uio goes all out
+    assign uio_out  = 8'b11111111; // port uio goes all out
+    assign uo_out[7:1] = 7'b0000000;
+    wire internal_rst; // Internal reset signal based on lower threshold state as well as external reset signal
+    wire internal_rst_n; // Internal reset signal based on lower threshold state as well as external reset signal
+    wire rst;
+    wire dly_high; // Internal reset signal based on lower threshold state as well as external reset signal
+    wire dff_high_qn; // Output D-FF of the high triggered FF
+    wire dly_low; // Internal reset signal based on lower threshold state as well as external reset signal
+    wire dff_low_q; // Output D-FF of the low triggered FF
+    wire coincidence_cont_q; // conincidence detection and rejection output
+    wire dff_output_q;
+    wire rst_dff_output;
+    wire dly_dff_rst_1;
+    wire dly_dff_rst_3;
+    wire dly_dff_rst_9;
+    wire dly_dff_rst_27;
+    wire dly_dff_output_q;
 
   // All output pins must be assigned. If not used, assign to 0.
-  assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
-  assign uio_out = 0;
-  assign uio_oe  = 0;
+    (* keep *) sg13g2_dlygate4sd2_1  dly0(.X(dly_low), .A(ui_in[0]));
+    (* keep *) sg13g2_dlygate4sd2_1  dly1(.X(dly_high), .A(ui_in[1]));
+    (* keep *) sg13g2_and2_1  and0(.X(internal_rst), .A(ui_in[0]), .B(rst_n));
+    (* keep *) sg13g2_dfrbp_1  dff_low(.CLK(dly_low),.RESET_B(internal_rst),.D(1'b1),.Q(dff_low_q));
+    (* keep *) sg13g2_dfrbp_1  dff_high(.CLK(dly_high),.RESET_B(internal_rst),.D(1'b1),.Q_N(dff_high_qn));
+    (* keep *) sg13g2_and2_1  and1(.X(coincidence_cont_q), .A(dff_high_qn), .B(dff_low_q));
+    //inverter inv0 (.in(internal_rst),.out(internal_rst_n));
+    (* keep *) sg13g2_inv_2  inv0(.Y(internal_rst_n), .A(internal_rst));
+    //inverter inv1 (.in(rst_n),.out(rst));
+    (* keep *) sg13g2_inv_2  inv1(.Y(rst), .A(rst_n));
+    //(* keep *) sg13g2_dlygate4sd2_1  dly2(.X(dly_high), .A(ui_in[1]));
+    //delay_gate_sim dly2 (.in(ui_in[1]),.out(dly_high)); Whats that ?
+    (* keep *) sg13g2_dfrbp_1  dff_output(.CLK(internal_rst_n),.RESET_B(rst_dff_output),.D(coincidence_cont_q),.Q(dff_output_q));
+    //d_ff_async_reset dff_output (.clk(internal_rst_n),.rst_n(rst_dff_output),.d(coincidence_cont_q),.q(dff_output_q),.qn(dff_output_qn));
+    //(* keep *) sg13g2_dlygate4sd2_1  dly2(.X(dly_dff_rst_1), .A(dff_output_q));
+    delay_gate_sim dly3(.in(dff_output_q),.out(dly_dff_rst_1));
+    delay_gate_sim_triple dly4(.in(dff_output_q),.out(dly_dff_rst_3));
+    delay_gate_sim_nine dly5(.in(dff_output_q),.out(dly_dff_rst_9));
+    delay_gate_sim_twenty_seven dly6(.in(dff_output_q),.out(dly_dff_rst_27));
+    (* keep *) sg13g2_mux4_1 mux0(.A0(dly_dff_rst_1),.A1(dly_dff_rst_3),.A2(dly_dff_rst_9),.A3(dly_dff_rst_27),.S0(ui_in[2]),.S1(ui_in[3]),.X(dly_dff_output_q));
+    //mux_4_to_1 mux0(.A0(dly_dff_rst_1),.A1(dly_dff_rst_3),.A2(dly_dff_rst_9),.A3(dly_dff_rst_27),.S0(ui_in[2]),.S1(ui_in[3]),.X(dly_dff_output_q));
+    (* keep *) sg13g2_xnor2_1 xnor0(.Y(rst_dff_output), .A(rst), .B(dly_dff_output_q));
+    //xor_gate_level xor0(.a(rst_n),.b(dly_dff_output_q),.out(rst_dff_output));
+    
+
+  assign uo_out[0]=dff_output_q;
+  //assign uio_out = 0;
+  //assign uio_oe  = 0;
 
   // List all unused inputs to prevent warnings
-  wire _unused = &{ena, clk, rst_n, 1'b0};
+    wire _unused = &{ui_in[7:4], uio_in, ena, clk, 1'b0};
+
 endmodule
 
+
+module delay_gate_sim (
+    input  wire in,
+    output wire out
+);
+
+    // Continuous assignment with a 0.45 ns delay
+    (* keep *) sg13g2_dlygate4sd2_1  dly0(.X(out), .A(in));
+
+endmodule
+
+module delay_gate_sim_triple (
+    input  wire in,
+    output wire out
+);
+  wire delay_1;
+  wire delay_2;
+
+    // Continuous assignment with a 1.35 ns delay
+    delay_gate_sim dly7 (.in(in), .out(delay_1));
+    delay_gate_sim dly8 (.in(delay_1), .out(delay_2));
+    delay_gate_sim dly9 (.in(delay_2), .out(out));
+endmodule
+
+module delay_gate_sim_nine (
+    input  wire in,
+    output wire out
+);
+  wire delay_1;
+  wire delay_2;
+
+    // Continuous assignment with a 4.05 ns delay
+    delay_gate_sim_triple dly10 (.in(in), .out(delay_1));
+    delay_gate_sim_triple dly11 (.in(delay_1), .out(delay_2));
+    delay_gate_sim_triple dly12 (.in(delay_2), .out(out));
+endmodule
+
+
+module delay_gate_sim_twenty_seven (
+    input  wire in,
+    output wire out
+);
+  wire delay_1;
+  wire delay_2;
+
+    // Continuous assignment with a 12.15 ns delay
+    delay_gate_sim_nine dly13 (.in(in), .out(delay_1));
+    delay_gate_sim_nine dly14 (.in(delay_1), .out(delay_2));
+    delay_gate_sim_nine dly15 (.in(delay_2), .out(out));
+endmodule

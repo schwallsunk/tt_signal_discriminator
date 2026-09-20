93721a22288e88036ac54032f38730daf8115746
schwallsunk <124298837+schwallsunk@users.noreply.github.com>
2026-09-20 08:44:00 +0200

    Modify clock cycles in test for ui_in value
    Increased clock cycles for input value testing and added a wait period.

--- test/test.py
+++ test/test.py
@@ -29,7 +29,11 @@ async def test_project(dut):
     dut.ui_in.value = 1
 
     # Wait for one clock cycle to see the output values
-    await ClockCycles(dut.clk, 1)
+    await ClockCycles(dut.clk, 3)
+
+    dut.ui_in.value = 0
+
+    await ClockCycles(dut.clk, 20)
 
     # The following assersion is just an example of how to check the output values.
     # Change it to match the actual expected output of your module:
